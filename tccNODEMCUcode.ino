32c8bbf4a73c04dcef00e1f1995225b456f5460b
Matheus Lima <38122411+lmaths@users.noreply.github.com>
2019-04-25 01:04:20 -0300

    Add files via upload
    Firebase + Display LCD exibir nome do animal + dono

--- tccNODEMCUcode.ino
+++ tccNODEMCUcode.ino
@@ -1,17 +1,20 @@
-
-
+#include <LiquidCrystal.h>
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
+
+LiquidCrystal lcd(16,5,4,0,2,14);
 
 // Set these to run example.
 #define FIREBASE_HOST "nodemcu-8c8b9.firebaseio.com"
 #define FIREBASE_AUTH "NCto0QZk7wkMgd0mdhZKTCHihHFNqEcxd1Ra1M6f"
-#define WIFI_SSID "TP-LINK_6642"
-#define WIFI_PASSWORD "40462929"
-int lampada = 0;
+#define WIFI_SSID "matheuslima"
+#define WIFI_PASSWORD "M@theus123000"
+
+
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2);
 
   // connect to wifi.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -24,23 +27,48 @@ void setup() {
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
   
-  pinMode(16, OUTPUT);
-  digitalWrite(16, 0);
+  
+
   
   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
  
-   Firebase.setInt("lampada", 0);
+   Firebase.setString("lampada", "Matheus");
+   Firebase.setString("nomeCachorro", "Lara");
    
   
 }
 
 
 void loop() {
-lampada = Firebase.getInt("lampada");
-Firebase.setInt("Usuarios", lampada);
 
-digitalWrite(16, Firebase.getInt("Usuarios"));
+lcd.clear();
+lcd.setCursor(3, 0);
+
+String name = Firebase.getString("lampada");
+String teste = Firebase.getString("nomeCachorro");
+
+Serial.print(name);
+lcd.print(name);
+
+ lcd.setCursor(3, 1);
+  lcd.print(teste);
+  delay(5000);
+
+
+  for (int posicao = 0; posicao < 3; posicao++)
+  {
+    lcd.scrollDisplayLeft();
+    delay(300);
+  }
+   
+  //Rolagem para a direita
+  for (int posicao = 0; posicao < 6; posicao++)
+  {
+    lcd.scrollDisplayRight();
+    delay(300);
+  }
+
 
 
 
