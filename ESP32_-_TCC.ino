dda10104efc33ac50201a0c2ef1549494a960922
Matheus Lima <38122411+lmaths@users.noreply.github.com>
2019-08-30 22:47:32 -0300

    alimentacao

--- ESP32_-_TCC.ino
+++ ESP32_-_TCC.ino
@@ -1,9 +1,12 @@
+#include <ArduinoJson.h>
+
 
 #include <Wire.h> //lib para conexao de portas i2c
 #include <WiFi.h> //lib de wifi
 #include "RTClib.h" //lib para modulo de relogio
 #include <IOXhop_FirebaseESP32.h>
 #include <LiquidCrystal_I2C.h>
+
 
 
 // Set these to run example.
@@ -19,9 +22,10 @@ RTC_DS3231 rtc;
 int totalColumns = 16;
 int totalRows = 2;
 
-int horaAlimentacao[3], minutoAlimentacao[3], demosComida[3];
+ // passar a quantidade do firebase
 
-
+int releMotor = 15;
+int demosComida1, demosComida2;
 
 
 LiquidCrystal_I2C lcd(0x27, totalColumns, totalRows);
@@ -33,18 +37,11 @@ char daysOfTheWeek[7][12] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta",
 
 
 void setup() {
+  pinMode(releMotor, OUTPUT); 
+  digitalWrite(releMotor, HIGH);
 
-   horaAlimentacao[0] = 14; 
-   minutoAlimentacao[0] = 27;
-   demosComida [0] = 0;   
-
-   horaAlimentacao[1] = 14;
-   minutoAlimentacao[1]= 20;
-   demosComida [1] = 0;
-   horaAlimentacao[2] = 14;
-   minutoAlimentacao[2] = 25;
-    demosComida [2] = 0;
-
+    demosComida1 = 0;
+    demosComida2 = 0;
   
    Serial.begin(9600);
    lcd.init(); 
@@ -60,10 +57,10 @@ void setup() {
   if (rtc.lostPower()) {
     Serial.println("RTC lost power, lets set the time!");
     // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+   rtc.adjust(DateTime(2019, 8, 30, 17, 25                    , 0));
   }
 
   
@@ -85,29 +82,130 @@ delay (3000);
   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
  
-   demosComida[0] = 1;
-    demosComida[1] = 1;
-    demosComida[2] = 1;
 
 }
 
 void loop() {
 
+   
   
-  
-      Firebase.setString("lampada", "Matheus");
-   Firebase.setString("nomeCachorro", "Lara");
-    DateTime now = rtc.now();
+   DateTime now = rtc.now();
 
    int horaAtual = now.hour();
    int minutoAtual = now.minute();
+   
+    delay(3000);
+ //int quantidadeAlimentacao = Firebase.getInt("qntAlimentar");
 
-   if(horaAtual == horaAlimentacao[0] && minutoAtual == minutoAlimentacao[0] && demosComida[0] == 0) {
+ int horaAlimentacao1 = Firebase.getInt("/horaAlimentacao");
+ int minutoAlimentacao1 = Firebase.getInt("/minutoAlimentacao");
+ int horaAlimentacao2 = Firebase.getInt("/horaAlimentacaoDois");
+ int minutoAlimentacao2 = Firebase.getInt("/minutoAlimentacaoDois");
+
+ Serial.println(horaAlimentacao1);
+ Serial.println(minutoAlimentacao1);
+ Serial.println(horaAlimentacao2);
+ Serial.println(minutoAlimentacao2);
+ delay (1000);
+
+ //Serial.println(quantidadeAlimentacao);
+  //int horaAlimentacao[quantidadeAlimentacao], minutoAlimentacao[quantidadeAlimentacao], demosComida[quantidadeAlimentacao];
+
+ /*int i = 0;
+  while(i < quantidadeAlimentacao) { //setar os horarios e alimentar
+  int teste = Firebase.get("/horarios/");
+   Serial.println(teste);
+    
+    horaAlimentacao[i] = 2;
+    Serial.println(horaAlimentacao[i]);
+    
+    i = i + 1;
+  } */
+   
+  
+    if(horaAtual == horaAlimentacao1 && minutoAtual == minutoAlimentacao1 && demosComida1 == 0) {
+
+      digitalWrite(releMotor, LOW);
+      delay(20000);
+      digitalWrite(releMotor, HIGH);
+
+      demosComida1 = 1;
     Serial.print("alimentou");
-    demosComida[0] = 1;
-    demosComida[1] = 1;
-    demosComida[2] = 1;
+  
    }
+
+   if(horaAtual == horaAlimentacao2 && minutoAtual == minutoAlimentacao2 && demosComida2 == 0) {
+      digitalWrite(releMotor, LOW);
+      delay(20000);
+      digitalWrite(releMotor, HIGH);
+
+      demosComida2 = 1;
+
+       Serial.print("alimentou 2");
+   }
+
+    if (demosComida1 == 0 || demosComida1 == 1 && demosComida2 == 1 ){
+      lcd.clear();
+     
+      
+      int Hora = (now.hour());
+      int Min = (now.minute());
+      String horario = "Hora: ";
+      String fim = "";
+
+     
+      String prox = "Prox: ";
+      String fimAlimentacao = prox + horaAlimentacao1 + ":" + minutoAlimentacao1;
+      
+        fim = (horario + Hora + ":" + Min);
+        lcd.setCursor(0,0);
+        lcd.print(fim);
+        lcd.setCursor(0,1);
+        lcd.print(fimAlimentacao);
+      
+  
+     
+   
+  
+  
+    Serial.print("Horário atual: ");
+   
+    Serial.print("Próxima alimentação: ");
+    Serial.print(horaAlimentacao1);
+    Serial.print("h:");
+    Serial.print(minutoAlimentacao1);
+    Serial.println("min");
+    Serial.println("  ");
+  }
+
+  if (demosComida1 == 1 && demosComida2 == 0){
+    
+    lcd.print("Alimentação: " + horaAlimentacao2 + minutoAlimentacao2);
+    
+    int Hora2 = (now.hour());
+      int Min2 = (now.minute());
+      String horario2 = "Hora: ";
+      String fim2 = "";
+
+     
+      String prox2 = "Prox: ";
+      String fimAlimentacao2 = prox2 + horaAlimentacao2 + ":" + minutoAlimentacao2;
+      
+        fim2 = (horario2 + Hora2 + ":" + Min2);
+        lcd.setCursor(0,0);
+        lcd.print(fim2);
+        lcd.setCursor(0,1);
+        lcd.print(fimAlimentacao2);
+      
+    
+    Serial.print("Próxima alimentação: ");
+    Serial.print(horaAlimentacao2);
+    Serial.print("h:");
+    Serial.print(minutoAlimentacao2);
+    Serial.println("min");
+    Serial.println("  ");
+  }
+
     
     Serial.print(now.year(), DEC);
     Serial.print('/');
@@ -125,14 +223,29 @@ void loop() {
     Serial.println();
 
 
-  if(horaAtual == 0 && minutoAtual == 0) { //reseta o verificador de alimentação a meia noite 
+    
+
+
+ /* if(horaAtual == 0 && minutoAtual == 0) { //reseta o verificador de alimentação a meia noite 
     for(byte i = 0; i <= demosComida[i]; i = i +1) {
       demosComida[i] = 0 ;
 
       Serial.print (demosComida[i]);
     }
+  } */
+
+  if (horaAtual == 0 && minutoAtual == 0){
+    demosComida1 = 0;
+    demosComida2 = 0; 
   }
+
+    delay (1000);
+
+
+
   
+
+ 
 
 
    
